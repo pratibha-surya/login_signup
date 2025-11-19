8dba0eea95b42bf0edef12a4cfb7e8a27dbcd4e0
pratibha-surya <suryavanshipratibha18@gmail.com>
2025-11-19 10:16:53 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -11,25 +11,28 @@ connectDB();
 
 const app = express();
 
-// Fix for __dirname in ES modules
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 
-// CORS for React dev server
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
 
-// API routes
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+
 app.use("/api/v1/auth", authRoute);
 
-// Serve frontend (React build)
+
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
+
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
